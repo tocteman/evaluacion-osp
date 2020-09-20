2100068ddcad370ad25eca5cf9d2fb1b1f1f098f
tocteman <juanfrancisco.vinueza@gmail.com>
2020-09-20 16:01:52 -0500

    cambio readme

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import '../src/assets/css/styles.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App min-h-screen flex">
+      <div className="w-1/4">
+      
+      </div>
+      <div className="w-3/4">
+
+      </div>
     </div>
   );
 }
