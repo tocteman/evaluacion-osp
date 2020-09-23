122443b68ac03455dc282eda30af80311a8d630f
tocteman <juanfrancisco.vinueza@gmail.com>
2020-09-23 09:36:44 -0500

    editModal fix

--- src/pages/Peliculas.tsx
+++ src/pages/Peliculas.tsx
@@ -21,7 +21,7 @@ const Peliculas:FunctionComponent<PeliculasProps> = ({data}) => {
   const EditIcon:FunctionComponent<EditIconProps> = ({editData}) => {
     return (
       <svg onClick={()=> {
-        setIsModalOpen("edit"); setModalData(data)}} className="w-6 h-6 cursor-pointer hover:text-indigo-800 underline" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
+        setIsModalOpen("edit"); setModalData(editData)}} className="w-6 h-6 cursor-pointer hover:text-indigo-800 underline" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
     )
   }
 
